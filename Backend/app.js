967ef3dbf03b71faa40e0926459cdc2fba80e726
jagannathking <sethij832@gmail.com>
2025-03-17 22:17:54 +0530

    debugging

---
+++ Backend/app.js
@@ -0,0 +1,42 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js";
+import jobRoute from "./routes/job.route.js";
+import applicationRoute from "./routes/application.route.js";
+
+dotenv.config();
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// const corsOptions = {
+//   // origin: [process.env.FONTEND_IND_POINT],
+//   origin: process.env.FONTEND_IND_POINT || "*",
+//   credentials: true,
+// };
+
+// console.log("cors error",process.env.FONTEND_IND_POINT)
+// app.use(cors(corsOptions));
+
+app.use(cors());
+
+
+const PORT = process.env.PORT || 5001;
+
+ 
+//api's
+
+app.use("/api/user", userRoute);
+app.use("/api/company", companyRoute);
+app.use("/api/job", jobRoute);
+app.use("/api/application", applicationRoute);
+
+
+export default app
--- Backend/index.js
+++ Backend/index.js
@@ -1,43 +1,10 @@
-import express from "express";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-import dotenv from "dotenv";
+import app from './app.js'
+import dotenv from 'dotenv'
 import connectDB from "./utils/db.js";
-import userRoute from "./routes/user.route.js";
-import companyRoute from "./routes/company.route.js";
-import jobRoute from "./routes/job.route.js";
-import applicationRoute from "./routes/application.route.js";
 
 dotenv.config();
-const app = express();
 
-//middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-
-// const corsOptions = {
-//   // origin: [process.env.FONTEND_IND_POINT],
-//   origin: process.env.FONTEND_IND_POINT || "*",
-//   credentials: true,
-// };
-
-// console.log("cors error",process.env.FONTEND_IND_POINT)
-// app.use(cors(corsOptions));
-
-app.use(cors());
-
-
-const PORT = process.env.PORT || 5001;
-
- 
-//api's
-
-app.use("/api/user", userRoute);
-app.use("/api/company", companyRoute);
-app.use("/api/job", jobRoute);
-app.use("/api/application", applicationRoute);
-
+export default app
 app.listen(PORT, () => {
   connectDB();
   console.log(`Server is running on port ${PORT}`);
