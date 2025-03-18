c51b48cd1f6816e518dcd61ea2a7488e9884c987
jagannathking <sethij832@gmail.com>
2025-03-18 10:47:41 +0530

    cors error debugging

--- Backend/app.js
+++ Backend/app.js
@@ -16,13 +16,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+// const corsOptions = {
+//   origin: ["https://job-portal-tfzw.vercel.app"],
+//   credentials: true,
+// };
+// app.use(cors(corsOptions));
+
+
+// Allow requests only from your frontend domain
 const corsOptions = {
-  origin: ["https://job-portal-tfzw.vercel.app"],
-  credentials: true,
-};
-app.use(cors(corsOptions));
-
-
+    origin: "https://job-portal-tfzw.vercel.app/", 
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
+  };
+  
+  app.use(cors(corsOptions));
 //api's
 
 app.use("/api/user", userRoute);
